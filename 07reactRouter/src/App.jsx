96d975baf12a28220ecd064264ccc4d260259744
AMRIT <amrit.raj2023@vitstudent.ac.in>
2025-09-13 01:45:15 +0530

    React Router Dom

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-gray-600 p-4'>Hi its Amrit here ...</h1>
+
+    </>
+  )
+}
+
+export default App
